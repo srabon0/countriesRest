5860cbacf08d66016aa7466c1108c6bf9d65c0e7
Srabon Emam <47981853+srabon0@users.noreply.github.com>
2022-02-23 01:37:44 +0000

    single country details added

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const loadCountries = () =>{
 const displayAllCountries = (countires) =>{
     const countryHolder = document.getElementById('container');
     countires.forEach( country => {
-            console.log(country);
+           
             const div = document.createElement('div');
             div.innerHTML = `
             <h3> ${country.name} </h3>
             <p>${country.capital}</p>
             <img class="flag" src="${country.flags.png}" alt="this is ${country.name}" >
-            <button onclick='loadCountryDetails()' >Country details </button>`
+            <button onclick="loadCountryDetails('${country.name}')" >Country details </button>`
             // const h3 = document.createElement('h3');
             // const p = document.createElement('p');
             // const img = document.createElement('img');
@@ -27,14 +27,28 @@ const displayAllCountries = (countires) =>{
             // div.appendChild(p);
             // div.appendChild(img);
 
-            countryHolder.appendChild(div); 
-           
-           
-            
-           
+            countryHolder.appendChild(div);
         
         
         } )
 }
 
 loadCountries();
+
+const loadCountryDetails = (name) =>{
+    const url =`https://restcountries.com/v2/name/${name}`;
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then (data => dataOftheCountry(data[0]))
+}
+const dataOftheCountry = (country) => {
+    console.log(country.name)
+    const single = document.getElementById('country-container');
+    single.innerHTML = `
+    <h1> ${country.name} </h1>
+    <h3>The population is ${ country.population } </h3>`
+    single.style.display = "block";
+
+    
+}
